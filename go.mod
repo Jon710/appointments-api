d19e200eca8ced21037682c2cb4b6fa23c26637b
João Luis Moraes <jonmoraesNL@gmail.com>
2023-08-31 01:21:11 -0300

    get + post patients

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module appointments-api
 
 go 1.15
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	github.com/golang/protobuf v1.5.3
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.9
+)
--- main.go
+++ main.go
@@ -1,23 +1,79 @@
 package main
 
 import (
-	"fmt"
-	// "net/http"
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
-type Person struct {
-	Name string
-	Age  int
-}
-
-func (p Person) PrintHelloWithStruct() {
-	fmt.Printf("Hello, my name is %s and I am %d years old\n", p.Name, p.Age)
+type Patient struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
 
 func main() {
-	p := Person{Name: "João", Age: 26}
+	db, err := sql.Open("postgres", "user=joaoluismoraes dbname=appointments password=999selva sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-	p.PrintHelloWithStruct()
+	// MUX Router
+	router := mux.NewRouter()
+	router.HandleFunc("/patients", getPatients(db)).Methods("GET")
+	// router.HandleFunc("/users/{id}", getUser(db)).Methods("GET")
+	router.HandleFunc("/patients", createPatient(db)).Methods("POST")
+	// router.HandleFunc("/users/{id}", updateUser(db)).Methods("PUT")
 
-	// http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8000", jsonContentTypeMiddleware(router)))
+}
+
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func getPatients(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT * FROM patients")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		patients := []Patient{}
+		for rows.Next() {
+			var p Patient
+			if err := rows.Scan(&p.ID, &p.Name, &p.Email); err != nil {
+				log.Fatal(err)
+			}
+			patients = append(patients, p)
+		}
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		json.NewEncoder(w).Encode(patients)
+	}
+}
+
+func createPatient(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var p Patient
+		json.NewDecoder(r.Body).Decode(&p)
+
+		err := db.QueryRow("INSERT INTO patients (name, email) VALUES ($1, $2) RETURNING id", p.Name, p.Email).Scan(&p.ID)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		json.NewEncoder(w).Encode(p)
+	}
 }
